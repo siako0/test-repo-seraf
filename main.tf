6ed98088bac6d9d275568425a37f941b8772fcdf
Emanuele Pannuccio <emanuele.pannuccio0@gmail.com>
2025-04-03 10:51:51 +0200

    feat: fix vul bucket gcp

--- main.tf
+++ main.tf
@@ -2,5 +2,15 @@ resource "google_storage_bucket" "example" {
   name     = "my-example-bucket"
   location = "US"
   acl      = "private"
-  # Nessuna configurazione di cifratura, Checkov segnalerà una violazione
+  versioning {
+    enabled = true
+  }
+  uniform_bucket_level_access = true
+  logging {
+    log_bucket = "logs-my-example-bucket"
+  }
+  
+  encryption {
+    default_kms_key = "projects/my-project/locations/global/keyRings/my-key-ring/cryptoKeys/my-key"
+  }
 }
